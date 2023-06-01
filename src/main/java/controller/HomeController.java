e5da10d4d6419c4afb1961d52eabbd35d66a0ea6
maituananh <maituananh12061998@gmail.com>
2023-06-01 20:29:26 +0700

    connect DB and get data from Table Product

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
@@ -10,19 +13,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import config.Connect;
+import model.Product;
+
 @WebServlet("/home")
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("vao trang home.jsp");
+		ArrayList<Product> productList = new ArrayList<Product>();
 		
-		// tạo 1 arraylist product có 5 products
-		ArrayList<String> productList = new ArrayList<String>();
-		productList.add("p1");
-		productList.add("p2");
-		productList.add("p3");
+		try {
+			Connection conn = Connect.openConnect();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("select `id`, `name`, `price`, `description`, `image` from products");
+			while (rs.next()) {
+				Product product = new Product();
+				product.setId(rs.getInt("id"));
+				product.setName(rs.getString("name"));
+				product.setDescription(rs.getString("description"));
+//				product.setPrice(rs.getString("price"));
+				product.setImage(rs.getString("image"));
+				
+				productList.add(product);
+			}
+			conn.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 		
 		request.setAttribute("ProductList", productList);
 		
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -7,17 +7,28 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+	crossorigin="anonymous">
 <title>Insert title here</title>
 </head>
 <body>
 	trang home shopee
-	<%-- 	<% ArrayList<String> productList = (ArrayList) request.getAttribute("ProductList"); %>
-	<% for (String product : productList) { %>
-	<%= product %>
-	<% } %> --%>
-
 	<c:forEach items="${ProductList}" var="product">
-	  ${product}
+		<div class="card" style="width: 18rem;">
+			<img src="..." class="card-img-top" alt="...">
+			<div class="card-body">
+				<h5 class="card-title">${product.name}</h5>
+				<p class="card-text">${product.price}.</p>
+				<a href="#" class="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
 	</c:forEach>
+	<script
+		src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+		crossorigin="anonymous"></script>
 </body>
 </html>
